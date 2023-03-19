1803b11df93b233793937e7d26f80edf71f56c97
OriginalStefikO <69142073+OriginalStefikO@users.noreply.github.com>
2023-03-19 18:06:09 +0100

    first commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  let rand = -1;
+  function getRand() {
+    fetch("../rand")
+      .then(d => d.text())
+      .then(d => (rand = parseInt(d)));
+  }
+</script>
+<main>
+<h1>Hello {rand}!</h1>
+<button on:click={getRand}>Get a random number</button>
+</main>
---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+os.chdir("frontend")
+os.system("npm run build")
+os.chdir("../")
+
+from typing import Union
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
+import uvicorn
+
+import random
+
+app = FastAPI()
+
+app.mount("/front", StaticFiles(directory="frontend/dist", html=True), name="front")
+app.mount("/build", StaticFiles(directory="frontend/dist/assets"), name="build")
+
+@app.get("/rand")
+async def rand():
+    return random.randint(0, 100)
+
+@app.get('/')
+async def front():
+   return RedirectResponse(url='front', status_code=302)
