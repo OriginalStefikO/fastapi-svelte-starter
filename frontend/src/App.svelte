44769e77800cd620e287853eb9a8aba33dbec137
OriginalStefikO <69142073+OriginalStefikO@users.noreply.github.com>
2023-04-26 15:18:14 +0200

    Added link to this Github

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from './assets/vite.svg'
+  import githubLogo from './assets/github-mark-white.svg'
   import Counter from './lib/Counter.svelte'
 </script>
 
@@ -12,16 +13,15 @@
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
+    <a href="https://github.com/OriginalStefikO/fastapi-svelte-starter" target="_blank" rel="noreferrer">
+      <img src={githubLogo} class="logo svelte" alt="Github Logo" />
+    </a>
   </div>
   <h1>Vite + Svelte</h1>
 
   <div class="card">
     <Counter />
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
 
   <p class="read-the-docs">
     Click on the Vite and Svelte logos to learn more
